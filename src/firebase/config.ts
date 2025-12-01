f9e728828897080e93a7b78a2c856c3d88ac5355
SYNC TECH Solutions <synctechire@gmail.com>
2025-12-01 17:42:32 +0000

    i huess i will go with github local so before i do it can you make sure

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-9075306461-1298d",
-  "appId": "1:941915984146:web:4db7714a2726c4bbd1a3cb",
-  "apiKey": "AIzaSyAnaTDfoIZtsI4-znqaMGlIXfpSHzhfvPs",
-  "authDomain": "studio-9075306461-1298d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "941915984146"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
