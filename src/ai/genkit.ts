e29e536afd131437151db6101ca7e9f0c4ddaed0
SYNC TECH Solutions <synctechire@gmail.com>
2025-12-11 18:38:32 +0000

    Try fixing this error: `Console Error: GenkitError: INVALID_ARGUMENT: Mu

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -7,11 +7,6 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI({
-      models: {
-          'gemini-1.5-flash': {
-              default: true,
-          }
-      },
       apiVersion: 'v1beta'
   })],
   flowStateStore: 'firebase',
